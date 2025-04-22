12bd9c49489ef581a744494306df0ee14a1106cd
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-04-23 00:01:36 +0200

    Test setup adjustments

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,13 +6,17 @@ namespace TTBooking\Stateful\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use TTBooking\Stateful\Facades;
+use TTBooking\Stateful\StatefulApiServiceProvider;
 use TTBooking\Stateful\StatefulServiceProvider;
 
 abstract class TestCase extends OrchestraTestCase
 {
     protected function getPackageProviders($app): array
     {
-        return [StatefulServiceProvider::class];
+        return [
+            StatefulServiceProvider::class,
+            StatefulApiServiceProvider::class,
+        ];
     }
 
     protected function getPackageAliases($app): array
@@ -20,6 +24,7 @@ abstract class TestCase extends OrchestraTestCase
         return [
             'Stateful' => Facades\Stateful::class,
             'SFSerializer' => Facades\Serializer::class,
+            'SFClient' => Facades\Client::class,
             'SFState' => Facades\State::class,
         ];
     }
