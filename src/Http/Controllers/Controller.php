397b55cfacb3d2b5ccd1a0160430d74e75f3db83
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-06-19 15:38:42 +0200

    Serialize result/state and output as JSON from controller

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -4,22 +4,27 @@ declare(strict_types=1);
 
 namespace TTBooking\Stateful\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
-use TTBooking\Stateful\Contracts\Result;
 use TTBooking\Stateful\Facades\Stateful;
-use TTBooking\Stateful\State;
 
 class Controller extends BaseController
 {
-    public function query(string $service, string $query): Result
+    public function query(string $service, string $query): JsonResponse
     {
         $service = Stateful::service($service);
 
-        return $service->query($service->newQuery($query));
+        return JsonResponse::fromJsonString(
+            $service->serialize($service->query($service->newQuery($query)))
+        );
     }
 
-    public function state(string $service, string $state, ?string $closest = null): State
+    public function state(string $service, string $state, ?string $closest = null): JsonResponse
     {
-        return Stateful::service($service)->get($state);
+        $service = Stateful::service($service);
+
+        return JsonResponse::fromJsonString(
+            $service->serialize($service->get($state))
+        );
     }
 }
